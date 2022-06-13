63ed10e55bb33d1180c14565f5868bad45c830ee
hajikenji <mademoiselle3824@gmail.com>
2022-06-13 10:56:33 +0900

    scafoldで起動まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_013951) do
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
